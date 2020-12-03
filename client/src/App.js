ecb8f2c1a26c338faf8bfc1dbb05ab887fef4797
Bibiana Souza <souzabibiana@hotmail.com>
2020-12-03 15:34:55 -0800

    Initial folder structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './styles/app.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      {/* routes would go here :)  */}
+    </div>
+  );
+}
+
+export default App;
