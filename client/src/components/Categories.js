98077b3bae45c297bde88fcff0026b021023a8a7
Bibiana Souza <souzabibiana@hotmail.com>
2020-12-04 09:07:56 -0800

    adding icons

---
+++ client/src/components/Categories.js
@@ -0,0 +1,30 @@
+import homeIcon from './../assets/Home.png';
+import cartIcon from './../assets/Tab.png';
+import profileIcon from './../assets/Tab (1).png';
+import yourFavoritesHeader from './../assets/Title.png';
+import discountBanner from './../assets/Group 12.png';
+import category1 from './../assets/Item_Card.png';
+import category2 from './../assets/Item_Card (1).png';
+import category3 from './../assets/Item_Card (2).png';
+import category4 from './../assets/Item_Card (3).png';
+import category5 from './../assets/Item_Card (4).png';
+import category6 from './../assets/Item_Card (5).png';
+import searchInput from './../assets/Search.png';
+import notificationIcon from './../assets/Bell.png';
+import menuIcon from './../assets/menu 1.png';
+import favourites from './../assets/Favorites.png';
+
+
+function Categories() {
+
+    return (
+        <div className="categories">
+            <div className="header">
+                <h1>Hi Tammy!</h1>
+                <img src={bell}/>
+            </div>
+        </div>
+    )
+}
+
+export default Categories;
