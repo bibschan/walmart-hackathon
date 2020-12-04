13816cee5ea48e3d4327d4eadda77b3f89777c96
muhammad <muhammadrehman21@gmail.com>
2020-12-04 14:11:06 -0500

    added the image files and created static database for the cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import './styles/app.scss';
 import Header from './components/Header';
-import Footer from  './components/Footer/Footer'
+import Footer from  './components/Footer/Footer';
+import Categories from 
+import {Switch, 
+Route, 
+Link} from 'react-router-dom';
 
 function App() {
   return (
---
+++ client/src/components/Cart/Cart.js
@@ -0,0 +1,62 @@
+import './Cart.scss';
+import React, { Component } from 'react';
+import egg from '../../assets/egg.png';
+import soy from '../../assets/soysauce.png';
+import cucumber from '../../assets/cucumbers.jpeg';
+import tropicana from '../../assets/tropicana.png';
+
+
+
+
+
+
+export default  class Card extends Component() {
+
+    state = {
+        inventory: [
+            {
+                image: {egg},
+                name: 'No Name Grade A Large White Eggs',
+                description: '12 ct',
+                amount: '1',
+                price: '$2.69'
+            },
+            {
+                image: {soy},
+                name: 'Kikkoman Organic Soy Sauce',
+                description: '296 ml',
+                amount: '1',
+                price: '$5.29'
+            },
+            {
+                image: {cucumber},
+                name: 'Mini Cucumber',
+                description: '6 ct',
+                amount: '1',
+                price: '$5.49'
+            },
+            {
+                image: {tropicana},
+                name: 'Tropicana Watermelon Fruit Beverage',
+                description: '1.75 L',
+                amount: '1',
+                price: '$4.99'
+            }
+        ]
+    }
+     
+    handleCancel = () => {
+        hideModal();
+    }
+
+    handleNeighbour = (event) =>{
+        event.preventDefault();
+        hideModal();
+    }
+
+    render() {
+    return (
+        <P></P>
+        )
+    } 
+}
